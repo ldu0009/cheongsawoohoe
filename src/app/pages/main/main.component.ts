46c4039194500ae04187a2b6a3c28f8adf144393
woky <ldu0009@gmail.com>
2024-07-18 22:00:03 +0900

    save

--- src/app/pages/main/main.component.ts
+++ src/app/pages/main/main.component.ts
@@ -1,6 +1,16 @@
-import { Component, OnInit } from "@angular/core";
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from "@angular/core";
 import { Title } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: "app-main",
@@ -9,11 +19,34 @@ import { RouterModule } from "@angular/router";
   templateUrl: "./main.component.html",
   styleUrl: "./main.component.scss",
 })
-export class MainComponent implements OnInit {
+export class MainComponent implements OnInit, AfterViewInit {
+  @ViewChild("mainContainer") mainContainer:
+    | ElementRef<HTMLDivElement>
+    | undefined;
   constructor(private title: Title) {}
 
   ngOnInit(): void {
     this.title.setTitle("청사우회");
+  }
+
+  ngAfterViewInit(): void {
+    console.log(this.mainContainer?.nativeElement.scrollHeight);
+    console.log(ScrollTrigger.maxScroll(window));
+    gsap.to(["data-speed"], {
+      y: (i, el) => {
+        console.log(el);
+        const scrollHeight =
+          this.mainContainer?.nativeElement.scrollHeight ?? 0;
+        return (1 - parseFloat(el.getAttribute("data-speed"))) * scrollHeight;
+      },
+      ease: "none",
+      scrollTrigger: {
+        start: 0,
+        end: "max",
+        invalidateOnRefresh: true,
+        scrub: 0,
+      },
+    });
   }
 
   public videoPlay(e: Event) {
